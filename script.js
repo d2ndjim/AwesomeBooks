2f35c0a7ac231febaf0ff72d9c801562e4141df2
Lucas Shangrita <oustanilokman@gmail.com>
2022-03-14 20:54:30 +0300

    Add remove book function

--- script.js
+++ script.js
@@ -11,19 +11,31 @@ function Book(book_name, book_author, id) {
   this.id = id;
 }
 
-function showBook(title, author) {
+function showBook(title, author, id) {
   booksWrapper.innerHTML += `
   <div>
     <p>${title}</p>
     <p>${author}</p>
-    <button type='button'>Remove</button>
+    <button type='button' class="removeBtn${id}">Remove</button>
   </div>
   `
+  const removeButton = document.querySelectorAll('.remove-btn');
+  removeButton.forEach((elem) => {
+    elem.addEventListener('click', () => {
+      booksList = booksList.filter(book => book.id !== elem.id);
+      booksWrapper.innerHTML = '';
+      booksList.forEach((_book) => {
+        showBook(bookTitle.value, bookAuthor.value);
+      });
+    });
+  });
+
 }
+
 
 function addBooks() {
   addBk.addEventListener('click', () => {
-    booksList.push(new Book(bookTitle.value, bookAuthor.value))
+    booksList.push(new Book(bookTitle.value, bookAuthor.value, id = Date.now()))
     showBook(bookTitle.value, bookAuthor.value)
     bookTitle.value = '';
     bookAuthor.value = '';
@@ -31,3 +43,8 @@ function addBooks() {
 };
 
 addBooks()
+
+function removeBtn (){
+  
+ removeBtn.addEventListener('click', removeBtn);
+}
