880e0cab57f1e6e25e1ce847d50aa4c976d89ed7
Lekan <jimoridwanlekan@gmail.com>
2022-03-15 11:58:34 +0100

    fix filtering ID

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 const bookTitle = document.querySelector('#bookName');
 const bookAuthor = document.querySelector('#bookAuthor');
 const addBk = document.querySelector('.add-btn');
@@ -9,24 +10,28 @@ class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
-    this.id = title;
+    this.id = Date.now();
   }
 }
 
-function showBook(title, author) {
+function showBook(title, author, id) {
+  if (title === '' || title === '') {
+    return '';
+  }
+
   booksWrapper.innerHTML += `
   <div>
     <p>${title}</p>  
     <p>${author}</p>
-    <button type='button' class="remove-btn" id="${title}">Remove</button>
+    <button type='button' class="remove-btn" id="${id}">Remove</button>
   </div>`;
   const removeBtn = document.querySelectorAll('.remove-btn');
   removeBtn.forEach((button) => {
     button.addEventListener('click', () => {
-      booksList = booksList.filter((book) => book.id !== button.id);
+      booksList = booksList.filter((book) => book.id !== Number(button.id));
       booksWrapper.innerHTML = '';
       booksList.forEach((book) => {
-        showBook(book.title, book.author);
+        showBook(book.title, book.author, book.id);
       });
       localStorage.setItem('bookStorage', JSON.stringify(booksList));
     });
@@ -38,15 +43,16 @@ function localstorage() {
   if (localStorage !== null) {
     const store = JSON.parse(localStorage.getItem('bookStorage'));
     store.forEach((book) => {
-      showBook(book.title, book.author);
+      showBook(book.title, book.author, book.id);
     });
   }
 }
 
 function addBooks() {
   addBk.addEventListener('click', () => {
-    booksList.push(new Book(bookTitle.value, bookAuthor.value));
-    showBook(bookTitle.value, bookAuthor.value);
+    const currentBook = new Book(bookTitle.value, bookAuthor.value);
+    booksList.push(currentBook);
+    showBook(bookTitle.value, bookAuthor.value, currentBook.id);
     bookTitle.value = '';
     bookAuthor.value = '';
   });
