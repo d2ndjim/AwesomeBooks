6ba88124dfef27c7632acc4d9d3a0f83fcd9ccf8
Lekan <jimoridwanlekan@gmail.com>
2022-03-15 17:21:00 +0100

    fix linters errors

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /* eslint-disable consistent-return */
+/* eslint-disable max-classes-per-file */
+
 const bookTitle = document.querySelector('#bookName');
 const bookAuthor = document.querySelector('#bookAuthor');
 const addBk = document.querySelector('.add-btn');
 const booksWrapper = document.querySelector('.books-list');
-
 
 class Book {
   constructor(title, author) {
@@ -14,46 +15,46 @@ class Book {
 }
 class BooksdataBase {
   constructor() {
-    this.booksList = JSON.parse(localStorage.getItem("bookStorage")) || [];
+    this.booksList = JSON.parse(localStorage.getItem('bookStorage')) || [];
   }
 
   addBooks() {
-    addBk.addEventListener("click", () => {
+    addBk.addEventListener('click', () => {
       const currentBook = new Book(bookTitle.value, bookAuthor.value);
       this.booksList.push(currentBook);
       this.showBook(bookTitle.value, bookAuthor.value, currentBook.id);
-      bookTitle.value = "";
-      bookAuthor.value = "";
+      bookTitle.value = '';
+      bookAuthor.value = '';
     });
-    localStorage.setItem("bookStorage", JSON.stringify(this.booksList));
+    localStorage.setItem('bookStorage', JSON.stringify(this.booksList));
     this.localstorage();
   }
 
   showBook(title, author, id) {
-    if (title === ''|| author === '') {
+    if (title === '' || author === '') {
       return '';
     }
-   
+
     booksWrapper.innerHTML += `
     <div class="list-container">
       <p>"${title}" By ${author}</p>  
       <button type='button' class="remove-btn" id="${id}">Remove</button>
     </div>`;
 
-    const removeBtn = document.querySelectorAll(".remove-btn");
+    const removeBtn = document.querySelectorAll('.remove-btn');
     removeBtn.forEach((button) => {
-      button.addEventListener("click", () => {
+      button.addEventListener('click', () => {
         this.booksList = this.booksList.filter(
-          (book) => book.id !== Number(button.id)
+          (book) => book.id !== Number(button.id),
         );
-        booksWrapper.innerHTML = "";
+        booksWrapper.innerHTML = '';
         this.booksList.forEach((book) => {
           this.showBook(book.title, book.author, book.id);
         });
-        localStorage.setItem("bookStorage", JSON.stringify(this.booksList));
+        localStorage.setItem('bookStorage', JSON.stringify(this.booksList));
       });
     });
-    localStorage.setItem("bookStorage", JSON.stringify(this.booksList));
+    localStorage.setItem('bookStorage', JSON.stringify(this.booksList));
   }
 
   localstorage() {
@@ -66,6 +67,5 @@ class BooksdataBase {
   }
 }
 
-let book = new BooksdataBase();
+const book = new BooksdataBase();
 book.addBooks();
-
