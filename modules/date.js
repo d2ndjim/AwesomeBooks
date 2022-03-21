ac9c9f0fe11354744fe5323abe514880006d6b57
Lekan <jimoridwanlekan@gmail.com>
2022-03-21 14:11:37 +0100

    fix linters

--- modules/date.js
+++ modules/date.js
@@ -1,14 +1,14 @@
-const webDate = document.getElementById('date');
+/* eslint-disable no-undef */
+const webDate = document.getElementById("date");
 const date = () => {
   window.setInterval(
     () => {
-      /* eslint-disable no-undef */
       webDate.innerHTML = `<p>${luxon.DateTime.local().toLocaleString(
-        luxon.DateTime.DATETIME_MED_WITH_SECONDS,
+        luxon.DateTime.DATETIME_MED_WITH_SECONDS
       )}</p>`;
     },
     1000,
-    this,
+    this
   );
 };
 
